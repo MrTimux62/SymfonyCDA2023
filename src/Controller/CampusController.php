b406605100b4ff81131719b0e12eb3af09d0914e
Tim <tim.boulanger62137@gmail.com>
2023-10-03 17:26:20 +0200

    ville and campus create

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
 use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +17,39 @@ class CampusController extends AbstractController
      */
     public function list(CampusRepository $campusRepository): Response
     {
+        $campus = $campusRepository->findAll();
+
+        return $this->render('campus/list.html.twig', [
+            'campus' => $campus
+        ]);
+    }
+
+    /**
+     * @Route("/campus/create", name="campus_create")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $campus = new Campus();
+        $campus->setNom($request->query->get('campus_name'));
+
+        $entityManager->persist($campus);
+        $entityManager->flush();
+
+        return $this->json([
+            'success' => true,
+            'campus' => $campus
+        ]);
+    }
+
+    /**
+     * @Route("/campus/remove", name="campus_remove")
+     */
+    public function remove(Request $request, CampusRepository $campusRepository, EntityManagerInterface $entityManager): Response // WIP
+    {
+        $camp = $campusRepository->find($request->query->get('campus_id'));
+        $entityManager->remove($camp);
+        $entityManager->flush();
+
         $campus = $campusRepository->findAll();
 
         return $this->render('campus/list.html.twig', [
--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ville;
 use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +21,23 @@ class VilleController extends AbstractController
 
         return $this->render('ville/list.html.twig', [
             'villes' => $villes
+        ]);
+    }
+
+    /**
+     * @Route("/ville/create", name="ville_create")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $ville = new Ville();
+        $ville->setNom($request->query->get('city_name'))->setCodePostal($request->query->get('city_postalCode'));
+
+        $entityManager->persist($ville);
+        $entityManager->flush();
+
+        return $this->json([
+            'success' => true,
+            'city' => $ville
         ]);
     }
 }
