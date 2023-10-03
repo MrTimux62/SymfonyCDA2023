f8539eeeca6c27abf7f9903dcc4b9c26ac0f9d79
Harushann <harushan.aru@gmail.com>
2023-10-03 17:35:55 +0200

    Profil user créer

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\EditProfileType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function index(): Response
+    {
+        return $this->render('participant/index.html.twig');
+    }
+
+
+    /**
+     * @Route("modifier", name="profil_modifier")
+     */
+    public function editProfil(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Profil modifié !');
+            return $this->redirectToRoute('profil');
+        }
+        return $this->render('participant/edit.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
