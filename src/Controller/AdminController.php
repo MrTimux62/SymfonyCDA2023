34465842fcd972fa31305951ac55339c39689fd1
Tim <tim.boulanger62137@gmail.com>
2023-10-06 14:00:02 +0200

    admin part ( lieux wip )

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -30,7 +30,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin_home")
      */
-    public function list(): Response
+    public function home(): Response
     {
 
         if ($this->getUser())
@@ -51,5 +51,84 @@ class AdminController extends AbstractController
             'sorties' => $sorties,
             'lieux' => $lieux
         ]);
+    }
+
+    /**
+     * @Route("/admin/sortie/delete", name="admin_sortie_delete")
+     */
+    public function sortieDelete(Request $request): Response
+    {
+
+        if ($this->getUser())
+        {
+            if (!in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
+                return new Response('Vous ne pouvez pas accèder à cette page.', Response::HTTP_FORBIDDEN);
+            }
+        } else {
+            return new Response('Vous ne pouvez pas accèder à cette page.', Response::HTTP_FORBIDDEN);
+        }
+
+        $sortie = $this->sortieRepository->find($request->query->get('sortie_id'));
+        if ($sortie === null) {
+            return new Response('Cette sortie n\'existe pas.', Response::HTTP_NOT_FOUND);
+        }
+        $this->sortieRepository->remove($sortie, true);
+        $this->addFlash('warning', 'Sortie supprimée avec succès !');
+        return $this->redirectToRoute('admin_home');
+    }
+
+    /**
+     * @Route("/admin/participant/delete", name="admin_participant_delete")
+     */
+    public function participantDelete(Request $request): Response
+    {
+
+        if ($this->getUser())
+        {
+            if (!in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
+                return new Response('Vous ne pouvez pas accèder à cette page.', Response::HTTP_FORBIDDEN);
+            }
+        } else {
+            return new Response('Vous ne pouvez pas accèder à cette page.', Response::HTTP_FORBIDDEN);
+        }
+
+        $participant = $this->participantRepository->find($request->query->get('participant_id'));
+        if ($participant === null) {
+            return new Response('Ce participant n\'existe pas.', Response::HTTP_NOT_FOUND);
+        }
+        $this->participantRepository->remove($participant, true);
+        $this->addFlash('warning', 'Participant supprimée avec succès !');
+        return $this->redirectToRoute('admin_home');
+    }
+
+    /**
+     * @Route("/admin/participant/switch", name="admin_participant_switch")
+     */
+    public function participantSwitch(Request $request): Response
+    {
+
+        if ($this->getUser())
+        {
+            if (!in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
+                return new Response('Vous ne pouvez pas accèder à cette page.', Response::HTTP_FORBIDDEN);
+            }
+        } else {
+            return new Response('Vous ne pouvez pas accèder à cette page.', Response::HTTP_FORBIDDEN);
+        }
+
+        $participant = $this->participantRepository->find($request->query->get('participant_id'));
+        if ($participant === null) {
+            return new Response('Ce participant n\'existe pas.', Response::HTTP_NOT_FOUND);
+        }
+        if ($participant->isActif()) {
+            $participant->setIsActif(false);
+            $this->addFlash('warning', 'Participant désactivé avec succès !');
+        } else {
+            $participant->setIsActif(true);
+            $this->addFlash('warning', 'Participant activé avec succès !');
+        }
+        $this->participantRepository->add($participant, true);
+
+        return $this->redirectToRoute('admin_home');
     }
 }
