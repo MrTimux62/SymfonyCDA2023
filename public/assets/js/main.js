151feefe12f74127727a364bc68089d256020b39
Tim <tim.boulanger62137@gmail.com>
2023-10-09 10:23:38 +0200

    add tooltip

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -112,10 +112,13 @@
   /**
    * Initiate tooltips
    */
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
+
+  $(document).ready(function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  });
 
   /**
    * Initiate quill editors
